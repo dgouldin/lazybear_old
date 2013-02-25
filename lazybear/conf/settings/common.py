07582fb51efb32171146162d164bfdb35ae4af94
David Gouldin <david@gould.in>
2013-02-25 10:19:10 -0800

    Serving static media on prod from S3.

--- lazybear/conf/settings/common.py
+++ lazybear/conf/settings/common.py
@@ -84,3 +84,5 @@ DATABASES = {'default': dj_database_url.config()}
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+STATICFILES_STORAGE = DEFAULT_FILE_STORAGE
--- lazybear/conf/settings/prod.py
+++ lazybear/conf/settings/prod.py
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-MEDIA_URL = '/media/'
-STATIC_URL = '/static/'
+# AWS
+AWS_ACCESS_KEY_ID = '19CH43SSGM2GDWRXQB02'
+AWS_SECRET_ACCESS_KEY = 'hK/xG6yq6KDulsHwvp/p+LPs7+OtkeTGDfcvR23K'
+AWS_STORAGE_BUCKET_NAME = 'lazybear'
+
+MEDIA_URL = 'http://s3.amazonaws.com/%s/' % AWS_STORAGE_BUCKET_NAME
+STATIC_URL = MEDIA_URL
